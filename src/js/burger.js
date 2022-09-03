05c09b1473c14c3a26d29c799b0ae6ecca0155b6
maksymomelchuk <omelchukmaksym@gmail.com>
2022-09-03 13:29:21 +0300

    change js script

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,25 +1,3 @@
-// Custom scripts
-// Get the container element
-var linkContainer = document.getElementById('header-nav');
-
-// Get all buttons with class="btn" inside the container
-var links = linkContainer.getElementsByClassName('header-nav__link');
-
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener('click', function () {
-    var current = document.getElementsByClassName('active');
-
-    // If there's no active class
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace(' active', '');
-    }
-
-    // Add the active class to the current/clicked button
-    this.className += ' active';
-  });
-}
-
 (() => {
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
