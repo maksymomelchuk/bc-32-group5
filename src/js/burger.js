3f861e30fef09acfe9aea2205ac6dc94ea9741bd
maksymomelchuk <omelchukmaksym@gmail.com>
2022-09-03 12:48:03 +0300

    finish header

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,28 +1,32 @@
-// (() => {
-//   const refs = {
-//     openMenuBtn: document.querySelector('[data-menu-open]'),
-//     closeMenuBtn: document.querySelector('[data-menu-close]'),
-//     menu: document.querySelector('[data-menu]'),
-//     // body: document.querySelector('body'),
-//   };
+// Custom scripts
+// Get the container element
+var linkContainer = document.getElementById('header-nav');
 
-//   refs.openMenuBtn.addEventListener('click', toggleMenu);
-//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-//   refs.menuList.addEventListener('click', removeMenu);
+// Get all buttons with class="btn" inside the container
+var links = linkContainer.getElementsByClassName('header-nav__link');
 
-//   function toggleMenu() {
-//     refs.menu.classList.toggle('is-hidden');
-// refs.body.classList.toggle('no-scroll');
-//   }
-// })();
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', function () {
+    var current = document.getElementsByClassName('active');
+
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(' active', '');
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += ' active';
+  });
+}
 
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('.menu-open'),
-    closeMenuBtn: document.querySelector('.menu-close'),
-    menu: document.querySelector('.mob-menu'),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('.mobile-menu'),
     body: document.querySelector('body'),
-    menuList: document.querySelector('.mob-menu-list'),
+    menuList: document.querySelector('.mobile-menu-nav__list'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
