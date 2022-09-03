d97ad8e15c7d6aed4a68278386c7d2e386eb507e
maksymomelchuk <omelchukmaksym@gmail.com>
2022-09-03 16:50:07 +0300

    change modal window

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,6 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelector('.modal-open'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
